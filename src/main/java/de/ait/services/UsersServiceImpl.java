5d884913e760a8346fb69d89acaa838c65f05150
golderosDR <golderos.spr@gmail.com>
2023-05-25 14:55:24 +0200

    Init

---
+++ src/main/java/de/ait/services/UsersServiceImpl.java
@@ -0,0 +1,36 @@
+package de.ait.services;
+
+import de.ait.models.User;
+import de.ait.repositories.UsersRepository;
+
+import java.util.*;
+
+public class UsersServiceImpl implements UsersService {
+
+    private UsersRepository usersRepository;
+
+    public UsersServiceImpl(UsersRepository usersRepository) {
+        this.usersRepository = usersRepository;
+    }
+
+    @Override
+    public List<String> getNames() {
+        List<User> users = usersRepository.findAll();
+        List<String> names = new ArrayList<>();
+
+        for (User user : users) {
+            names.add(user.getFirstName());
+        }
+        return names;
+    }
+
+    @Override
+    public String getLastNameOfMostAging() {
+        List<User> users = usersRepository.findAll();
+        Map<Integer, String> userAge = new HashMap<>();
+
+        for (User user : users) {
+            userAge.put(user.getAge(), user.getLastName());
+        return userAge.get(Collections.max(userAge.keySet()));
+    }
+}
