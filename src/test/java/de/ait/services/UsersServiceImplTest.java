ed92a2942adf49c7d3d7a5fc25f3a39c7a1e2f0f
AlexGlushenkov <Glushenkov@i.ua>
2023-05-26 12:39:54 +0200

    Добавленны тесты

--- src/test/java/de/ait/services/UsersServiceImplTest.java
+++ src/test/java/de/ait/services/UsersServiceImplTest.java
@@ -1,5 +1,6 @@
 package de.ait.services;
 
+import de.ait.models.User;
 import de.ait.repositories.UsersRepositoryListImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,17 +11,41 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class UsersServiceImplTest {
-private UsersServiceImpl usersService;
+    private UsersServiceImpl usersService;
+    private UsersRepositoryListImpl usersRepositoryList;
+
     @BeforeEach
     void setUp() {
-        this.usersService = new UsersServiceImpl(new UsersRepositoryListImpl());
+        this.usersRepositoryList = new UsersRepositoryListImpl();
+        this.usersService = new UsersServiceImpl(this.usersRepositoryList);
     }
 
     @Test
     void getNames() {
         List<String> actual = usersService.getNames();
-        List<String> expected = new ArrayList<>(List.of("User1", "User2", "User3"));
+        List<String> expected = new ArrayList<>(List.of("fName1", "fName2", "fName3", "fName4", "fName5"));
         assertEquals(expected, actual);
 
     }
+
+
+    @Test
+    void containsInRepository_true() {
+        List<User> userList = usersRepositoryList.findAll();
+        boolean actual = usersService.containsInRepository(new User("fName3", "lName3", 30, 1.90));
+        assertTrue(actual);
+    }
+    @Test
+    void containsInRepository_false() {
+        List<User> userList = usersRepositoryList.findAll();
+        boolean actual = usersService.containsInRepository( new User("fName3", "lName3", 42, 1.91));
+        assertFalse(actual);
+    }
+/*
+    @Test
+    void getFullNameOfMinHeight() {
+        String actual;
+        String expected;
+        boolean
+    }*/
 }
