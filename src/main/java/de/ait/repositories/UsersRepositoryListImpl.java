0474c6cde6e7cadb63e0146465ec867a6beda34b
golderosDR <golderos.spr@gmail.com>
2023-05-26 11:47:54 +0200

    edit UsersRepositoryListImpl //TODO remove submenu branch 2, wrong parse in branch 1

--- src/main/java/de/ait/repositories/UsersRepositoryListImpl.java
+++ src/main/java/de/ait/repositories/UsersRepositoryListImpl.java
@@ -8,14 +8,18 @@ import java.util.List;
 public class UsersRepositoryListImpl implements UsersRepository {
     @Override
     public List<User> findAll() {
-        User user = new User("User1", "User1", 20, 1.81);
-        User user1 = new User("User2", "User2", 25, 1.85);
-        User user2 = new User("User3", "User3", 30, 1.90);
+        User user1 = new User("fName1", "lName1", 20, 1.81);
+        User user2 = new User("fName2", "lName2", 35, 1.85);
+        User user3 = new User("fName3", "lName3", 30, 1.90);
+        User user4 = new User("fName4", "lName4", 50, 1.60);
+        User user5 = new User("fName5", "lName5", 32, 1.99);
 
         List<User> users = new ArrayList<>();
-        users.add(user);
         users.add(user1);
         users.add(user2);
+        users.add(user3);
+        users.add(user4);
+        users.add(user5);
 
         return users;
     }
